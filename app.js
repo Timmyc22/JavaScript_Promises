2cb59938dbf73c93e270e0e929ca5ab7b610cad9
Timothy Coleman <Timbo97@yahoo.com>
2023-12-28 10:48:48 -0500

    complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
  *
- * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
+ * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with a failure object. The failure object includes a boolean success property and a string message property.
  */
 function getList() {
   return new Promise((resolve, reject) => {
@@ -15,10 +15,23 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+// Handle the resolved or rejected states of the promise
 
-// TODO: If the promise resolves with the list of hobbits
-// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
-// TODO: If the promise rejects with the failure object
-// Display the failure message in the paragraph element with id="error" (check index.html file)
+// Call getList
+getList()
+  // If the promise resolves with the list of hobbits
+  .then((hobbits) => {
+    // Render the list of hobbits as list items within the unordered list with id="list"
+    const listUl = document.getElementById("list");
+    hobbits.forEach((hobbit) => {
+      const li = document.createElement("li");
+      li.textContent = hobbit;
+      listUl.appendChild(li);
+    });
+  })
+  // If the promise rejects with the failure object
+  .catch((failure) => {
+    // Display the failure message in the paragraph element with id="error"
+    const errorParagraph = document.getElementById("error");
+    errorParagraph.textContent = failure.message;
+  });
